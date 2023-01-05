a17418cd6f90e143aec0a9fbf3f989f437c73749
Baxromjon <73574634+Baxromjon@users.noreply.github.com>
2023-01-05 18:23:33 +0500

    edit delete wish list

--- src/main/java/com/example/online_shop_back/repository/WishListRepository.java
+++ src/main/java/com/example/online_shop_back/repository/WishListRepository.java
@@ -3,14 +3,19 @@ package com.example.online_shop_back.repository;
 import com.example.online_shop_back.entity.WishList;
 import com.example.online_shop_back.payload.ApiResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, UUID> {
 
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
     @Query(nativeQuery = true, value = "DELETE\n" +
             "from wish_list\n" +
             "where id = :id")
