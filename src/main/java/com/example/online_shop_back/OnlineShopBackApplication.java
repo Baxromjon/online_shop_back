6d7c24b2a1e520d7fc38f0130f0bfcbf102ea35e
Baxromjon <73574634+Baxromjon@users.noreply.github.com>
2023-02-06 17:47:01 +0500

    edit measurement

--- src/main/java/com/example/online_shop_back/OnlineShopBackApplication.java
+++ src/main/java/com/example/online_shop_back/OnlineShopBackApplication.java
@@ -2,10 +2,17 @@ package com.example.online_shop_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OnlineShopBackApplication {
 
+    @GetMapping("/message")
+    public String message(){
+        return "Congrats! Successfully deployed";
+    }
     public static void main(String[] args) {
         SpringApplication.run(OnlineShopBackApplication.class, args);
     }
